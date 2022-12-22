80bf54b0cda7ef533d1a235abe2f84ad6c0b8b31
David Bannon <dbannon@internode.on.net>
2022-12-22 19:28:18 +1100

    update monitor scripts

---
+++ cbindings/package/scripts/check4update.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+QTV=6    # or 5 or 6
+
+REMOTE_pro="https://gitlab.com/freepascal.org/lazarus/lazarus/-/raw/main/lcl/interfaces/qt""$QTV""/cbindings/Qt""$QTV""Pas.pro"
+REMOTEfile="Qt""$QTV""Pas.pro"
+LOCALfile="libQtPas_src/cbindings/Qt""$QTV""Pas.pro"
+
+
+# https://raw.githubusercontent.com/davidbannon/libqt6pas/master/cbindings/Qt6Pas.pro
+
+wget "$REMOTE_pro"
+REMOTEpat=`grep VER_PAT "$REMOTEfile"`
+LOCALpat=`grep VER_PAT "$LOCALfile"`
+if [ "$REMOTEpat" == "$LOCALpat" ]; then
+	echo "all clear"
+    exit 0
+else
+	# this means we need to raise an issue
+	echo "hmm, seems we have issues"
+	echo "$REMOTEpat" >> result.text
+	cat result.text
+    exit 1
+fi
